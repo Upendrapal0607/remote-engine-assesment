05a8efbd61ad04b1cf3154f4cc540c75fb84ad09
Upendra Pal <up06072000@gmail.com>
2023-12-17 23:13:26 +0530

    added backend deploy url

--- Front-end/remote-engine/src/Service/api.js
+++ Front-end/remote-engine/src/Service/api.js
@@ -1,5 +1,4 @@
 
 // all two important api 
-
-export const user_api = "http://localhost:8080/user";
-export const developer_api = "http://localhost:8080/developer";
+export const user_api = "https://comfortable-bat-leg-warmers.cyclic.app//user";
+export const developer_api = "https://comfortable-bat-leg-warmers.cyclic.app//developer";
